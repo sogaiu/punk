39e58f4f19e296ab254c1dcc248027fcdb78caff
sogaiu <983021772@users.noreply.github.com>
2019-08-28 18:56:42 +0900

    Single-line row support with ellipsis

--- ui/src/punk/ui/views.cljs
+++ ui/src/punk/ui/views.cljs
@@ -4,8 +4,15 @@
 
 (defnc MapView [{:keys [data nav] :as props}]
   [pc/Table (merge props
-                   {:cols [[:key first [:div {:style {:flex 1 :overflow "auto"}}]]
-                           [:value second [:div {:style {:flex 3 :overflow "auto"}}]]]
+                   {:cols [[:key first [:div {:style {:flex 1
+                                                      :overflow "hidden"
+                                                      :text-overflow "ellipsis"
+                                                      :white-space "nowrap"
+                                                      :padding-right "5px"}}]]
+                           [:value second [:div {:style {:flex 3
+                                                         :overflow "hidden"
+                                                         :text-overflow "ellipsis"
+                                                         :white-space "nowrap"}}]]]
                     :on-entry-click (fn [key] (nav data key (get data key)))
                     :data data}
                    ;; remove nav from props
@@ -13,15 +20,24 @@
 
 (defnc CollView [{:keys [data nav] :as props}]
   [pc/Table (merge props
-                   {:cols [[:idx first [:div {:style {:flex 1 :overflow "auto"}}]]
-                           [:value second [:div {:style {:flex 11 :overflow "auto"}}]]]
+                   {:cols [[:idx first [:div {:style {:flex 1
+                                                      :overflow "hidden"
+                                                      :text-overflow "ellipsis"
+                                                      :white-space "nowrap"
+                                                      :padding-right "5px"}}]]
+                           [:value second [:div {:style {:flex 11
+                                                         :overflow "hidden"
+                                                         :text-overflow "ellipsis"
+                                                         :white-space "nowrap"}}]]]
                     :on-entry-click (fn [key] (nav data key (get data key)))
                     :data (map-indexed vector data)}
                    {:nav nil})])
 
 (defnc SetView [{:keys [data nav] :as props}]
   [pc/Table (merge props
-                   {:cols [[:value identity [:div {:overflow "auto"}]]]
+                   {:cols [[:value identity [:div {:overflow "hidden"
+                                                   :text-overflow "ellipsis"
+                                                   :white-space "nowrap"}]]]
                     :on-entry-click (fn [v] (nav data nil v))
                     :data data}
                    {:nav nil})])
